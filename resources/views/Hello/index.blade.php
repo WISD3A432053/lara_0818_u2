d927f0113015a1ba12e2efd282c958f35483ca2d
M511-49\admin <s3A432053@student.ncut.edu.tw>
2017-10-19 16:35:08 +0800

    編輯Hello/index.blade.php

--- resources/views/Hello/index.blade.php
+++ resources/views/Hello/index.blade.php
@@ -1,7 +1,5 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2017/10/19
- * Time: 下午 04:31
- */
+@extends('layouts.master')
+@section('title', 'Hello')
+@section('content')
+    <h1>This is {{ $name }}'s home.</h1>
+@endsection
