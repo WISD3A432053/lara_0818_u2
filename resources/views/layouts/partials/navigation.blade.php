48447c1ca3ffa10f27719345a6bb712537d315ce
M511-49\admin <s3A432053@student.ncut.edu.tw>
2017-10-19 16:26:52 +0800

    編輯 navigation.blade.php

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -1,7 +1,8 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2017/10/19
- * Time: 下午 04:23
- */
+<ul>
+    <li>
+        <a href="#">Item 1</a>
+    </li>
+    <li>
+        <a href="#">Item 2</a>
+    </li>
+</ul>
